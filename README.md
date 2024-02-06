02b7f720507359715a57f4c8347844bd7c6386db
wolvenfaust <frederico.2018@alunos.utfpr.edu.br>
2024-02-06 12:55:36 -0300

    Atualizando o range dos números

---
+++ README.md
@@ -0,0 +1,23 @@
+# Jogo do Número Secreto 🕵️‍♂️🔢
+
+Este é um jogo simples do número secreto desenvolvido em JavaScript e HTML. O objetivo do jogo é adivinhar o número secreto gerado aleatoriamente pelo computador.
+
+## Como Jogar
+
+1. Abra o arquivo `index.html` em seu navegador web.
+2. Você ouvirá uma mensagem de boas-vindas e será solicitado a escolher um número entre 1 e 10.
+3. Insira o seu palpite no campo de texto e pressione Enter ou clique no botão "Enviar Palpite".
+4. O jogo irá fornecer feedback se o número que você escolheu é muito alto ou muito baixo em relação ao número secreto.
+5. Continue a adivinhar até encontrar o número secreto.
+6. Uma vez que você acertar o número secreto, o jogo parabenizará você e oferecerá a opção de reiniciar para uma nova rodada.
+
+## Tecnologias Utilizadas
+
+- **HTML**: A estrutura básica da interface do usuário.
+- **CSS**: Estilização simples para uma experiência visual agradável.
+- **JavaScript**: A lógica por trás do jogo, incluindo a geração do número secreto, verificação dos palpites e interações de voz usando a API do ResponsiveVoice. 💻🎙️
+
+## Contribuindo
+
+Sinta-se à vontade para abrir problemas (issues) e enviar solicitações de pull (pull requests) para melhorias, correções de bugs ou novos recursos. Seja educado e construtivo em seus comentários. 🤝
+
---
+++ app.js
@@ -0,0 +1,69 @@
+// Variáveis globais
+let listaDeNumeroSorteados = [];
+const numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+// Função para exibir texto na tela com suporte a voz
+function exibirTextoNaTela(tag, texto) {
+    const campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
+}
+
+// Função para exibir a mensagem inicial do jogo
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+// Chama a função para exibir a mensagem inicial ao carregar a página
+exibirMensagemInicial();
+
+// Função para verificar o chute do jogador
+function verificarChute() {
+    const chute = parseInt(document.querySelector('input').value);
+
+    if (chute === numeroSecreto) {
+        exibirTextoNaTela('h1', 'Você acertou!');
+        const palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        const mensagemTentativas = `Você acertou o número secreto em ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        exibirTextoNaTela('p', chute > numeroSecreto ? 'O número secreto é menor' : 'O número secreto é maior');
+        tentativas++;
+        limparCampo();
+    }
+}
+
+// Função para gerar um número aleatório
+function gerarNumeroAleatorio() {
+    const numeroEscolhido = Math.floor(Math.random() * numeroLimite) + 1;
+
+    if (listaDeNumeroSorteados.length === numeroLimite) {
+        listaDeNumeroSorteados = [];
+    }
+
+    if (listaDeNumeroSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    } else {
+        listaDeNumeroSorteados.push(numeroEscolhido);
+        console.log(listaDeNumeroSorteados);
+        return numeroEscolhido;
+    }
+}
+
+// Função para limpar o campo de input
+function limparCampo() {
+    document.querySelector('input').value = '';
+}
+
+// Função para reiniciar o jogo
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
